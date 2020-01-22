8466b3eb11d6f88446f5c154ba58180e6f378a4a
fulls1z3 <burak.tasci@deliveryhero.com>
2020-01-22 01:51:06 +0100

    wip

--- nodejs/apps/frontend/src/app/app.component.ts
+++ nodejs/apps/frontend/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { map, take } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  title = 'frontend';
   ocean = '';
 
   constructor(private readonly http: HttpClient) {

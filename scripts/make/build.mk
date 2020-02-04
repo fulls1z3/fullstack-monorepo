b27b342e78a27231bb632ecf76fee60900483c7d
Burak Tasci <burak.tasci@deliveryhero.com>
2020-02-04 23:34:38 +0100

    chore: add dev/prod modes (#11)

--- scripts/make/build.mk
+++ scripts/make/build.mk
@@ -1,4 +1,7 @@
-.PHONY: build
+.PHONY: build build-prod
 
 build: ## Build docker image
 	docker-compose build
+
+build-prod: ## Build docker image (production)
+	docker-compose -f docker-compose.prod.yml build
